1f28273d3f5217db030f4e4e6b7c7f81a7d71627
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2023-06-15 21:57:39 +0900

    CPU,メモリ使用率を取得するコード

--- Resource-Manager/MainPage.xaml.cs
+++ Resource-Manager/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,14 +19,64 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Resource_Manager
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+
+	public sealed partial class MainPage : Page
+	{
+		private DispatcherTimer timer;
+		private int cpuCount;
+		private SystemCpuUsageReport prevCpuReport=SystemDiagnosticInfo.GetForCurrentSystem().CpuUsage.GetReport();
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+			cpuUsageText.Text = "now:0";
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			timer = new DispatcherTimer
+			{
+				Interval = TimeSpan.FromSeconds(1)
+			};
+			timer.Tick += CpuTimer_Tick;
+			timer.Start();
+		}
+
+		private void CpuTimer_Tick(object sender, object e)
+		{
+			cpuCount++;
+			var CpuReport = SystemDiagnosticInfo.GetForCurrentSystem().CpuUsage.GetReport();
+			var MemReport = SystemDiagnosticInfo.GetForCurrentSystem().MemoryUsage.GetReport();
+			var kernel= CpuReport.KernelTime - prevCpuReport.KernelTime;
+			var user = CpuReport.UserTime - prevCpuReport.UserTime;
+			var idle = CpuReport.IdleTime - prevCpuReport.IdleTime;
+			var cpuUsage = "user:" + user + " kernel:" + kernel + " idle:" + idle;
+			prevCpuReport = CpuReport;
+			var memUsage = "commited:" + FormatBytesToString(MemReport.CommittedSizeInBytes) + " avail:" + FormatBytesToString(MemReport.AvailableSizeInBytes) + " total:" + FormatBytesToString(MemReport.TotalPhysicalSizeInBytes);
+
+			cpuUsageText.Text = "now:" + cpuCount.ToString() + "\nCPU:" + cpuUsage + "\nMEM:" + memUsage + "\nCPU%"+((user+kernel)/(user+kernel+idle));
+			
+		}
+		void getCpuFrequency(){
+			var cpu = new Windows.System.ProcessorArchitecture();
+			var report = ProcessDiagnosticInfo.GetForCurrentProcess().CpuUsage.GetReport();
+		}
+
+		string FormatBytesToString(ulong bytes)
+		{
+			string[] suffixes = { "B", "KB", "MB", "GB", "TB", "PB" };
+			int counter = 0;
+			decimal number = bytes;
+			while (Math.Round(number / 1024) >= 1)
+			{
+				number = number / 1024;
+				counter++;
+			}
+			
+			return string.Format("{0:n1}{1}", number, suffixes[counter]);
+		}
+	}
 }
